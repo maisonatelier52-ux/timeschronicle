1dfed53110203f9e3f1e2afb20da2af484c83743
wiresavvy <maisonatelier52@gmail.com>
2026-01-17 10:37:51 +0530

    [Fix]

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import Header from "./components/Header";
+import Header from "./components/header/Header";
 import Footer from "./components/Footer";
 import localFont from "next/font/local";
 
