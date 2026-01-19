097040e5bb1b27f8c080900c3ff51d6a0da1cf03
wiresavvy <maisonatelier52@gmail.com>
2026-01-19 14:49:53 +0530

    [Fix] image

--- app/components/MoreRecent.jsx
+++ app/components/MoreRecent.jsx
@@ -48,6 +48,7 @@ export default function MoreRecent({articles}) {
                         <img
                             src={article.image}
                             alt={article.title}
+                            loading="lazy"
                             className="
                             w-full h-full object-cover
                             transition-transform duration-300
@@ -275,6 +276,7 @@ export default function MoreRecent({articles}) {
                                 <img
                                     src={article.image}
                                     alt={article.title}
+                                    loading="lazy"
                                     className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-[1.04]"
                                 />
 
