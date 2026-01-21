4cfb4af2739d76c6b9404ae0330749b906ae4c2e
wiresavvy <maisonatelier52@gmail.com>
2026-01-21 18:17:26 +0530

    [Add] redirection in pages

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,26 @@ const nextConfig = {
         permanent: true,
       },
       {
+        source: "/julio",
+        destination: "/julio-herrera-velutini/julio-herrera-velutini-banking-profile",
+        permanent: true,
+      },
+      {
+        source: "/julio-herrera",
+        destination: "/julio-herrera-velutini/julio-herrera-velutini-banking-profile",
+        permanent: true,
+      },
+      {
+        source: "/herrera-velutini",
+        destination: "/julio-herrera-velutini/julio-herrera-velutini-banking-profile",
+        permanent: true,
+      },
+      {
+        source: "/julio-herrera-velutini-profile",
+        destination: "/julio-herrera-velutini/julio-herrera-velutini-banking-profile",
+        permanent: true,
+      },
+      {
         source: "/category",
         destination: "/category/business",
         permanent: true,
@@ -15,6 +35,11 @@ const nextConfig = {
       {
         source: "/news",
         destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/author",
+        destination: "/author/charles-williams",
         permanent: true,
       },
     ];
